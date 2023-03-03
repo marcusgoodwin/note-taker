bf869a51037f0cb45c1532723b38e567fb93c06b
unknown <mwgoodwin2000@gmail.com>
2023-03-03 03:11:19 -0700

    ignore

--- notes/notes.js
+++ notes/notes.js
@@ -1,33 +1,39 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs").promises;
+const path = require("path");
 
-function updateDb(id, notesArray) {
-    const deletedNote = id;
-    for (let i = 0; i < notesArray.length; i++) {
-      if (deletedNote === notesArray[i].id) {
-        notesArray.splice(i, 1);
-        fs.writeFileSync(
+async function updateDb(id, notesArray) {
+  const deletedNote = id;
+  for (let i = 0; i < notesArray.length; i++) {
+    if (deletedNote === notesArray[i].id) {
+      notesArray.splice(i, 1);
+      try {
+        await fs.writeFile(
           path.join(__dirname, "../db/db.json"),
-          JSON.stringify({notes: notesArray}, null, 2), err => {
-            if (err) {
-              throw err;
-            }
-          });
+          JSON.stringify({ notes: notesArray }, null, 2)
+        );
+      } catch (error) {
+        console.error(error);
       }
+      break;
     }
   }
+}
 
-  function createNewNote(body, notesArray) {
-    const newNote = body
-    notesArray.push(newNote);
-    fs.writeFileSync(
+function createNewNote(body, notesArray) {
+  const newNote = body;
+  notesArray.push(newNote);
+  try {
+    fs.writeFile(
       path.join(__dirname, "../db/db.json"),
-      JSON.stringify({notes: notesArray}, null, 2)
+      JSON.stringify({ notes: notesArray }, null, 2)
     );
-    return newNote;
-  };
+  } catch (error) {
+    console.error(error);
+  }
+  return newNote;
+}
 
-  module.exports = {
-    updateDb,
-    createNewNote,
-  }; 
+module.exports = {
+  updateDb,
+  createNewNote,
+};

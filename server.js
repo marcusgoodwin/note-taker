b9afe63024df92ac484caff47479fab098f7ce0a
unknown <mwgoodwin2000@gmail.com>
2023-03-03 01:13:44 -0700

    adding server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server now listening on port ${PORT}`);
+});
